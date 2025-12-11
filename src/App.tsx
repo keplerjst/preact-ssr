6269b3986ab1af5b035a97bec918263de4601282
Kepler Jay <keplerjst@gmail.com>
2025-12-11 09:35:10 +0900

    use preact-iso on routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-import Router from 'preact-router'
+import { ErrorBoundary, LocationProvider, Router } from 'preact-iso'
 import About from './app/About'
 import Home from './app/Home'
 
 const App = ({ path }: { path?: string }) => {
   return (
-    <Router url={path}>
-      <Home path="/" />
-      <About path="/about" />
-    </Router>
+    <LocationProvider>
+      <ErrorBoundary>
+        <Router>
+          <Home path="/" />
+          <About path="/about" />
+        </Router>
+      </ErrorBoundary>
+    </LocationProvider>
   )
 }
 
